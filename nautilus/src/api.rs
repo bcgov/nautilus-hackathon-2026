45b3b7a529536722912b64c3af16771d72fb45c6
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2026-01-20 14:24:37 -0800

    chore: arg parsing

--- nautilus/src/api.rs
+++ nautilus/src/api.rs
@@ -2,6 +2,9 @@ use rocket::{Build, Rocket};
 
 pub mod index;
 
+// The [launch] annotation generates a `main` method for this module
+#[launch]
 pub fn launch_webserver() -> Rocket<Build> {
     rocket::build().mount("/", routes![index::index])
 }
+
