721e27baf83c49863748f018079d6383a83f5c80
Dylan Leard <dylan.1.leard@gov.bc.ca>
2026-01-20 17:01:00 -0800

    chore: add some structs & implementation for returned data

--- nautilus/src/github.rs
+++ nautilus/src/github.rs
@@ -6,8 +6,28 @@ use serde_json;
 use reqwest::Error;
 use reqwest::header::{USER_AGENT};
 
+#[derive(Debug)]
+pub struct PrCommitData {
+    title: String,
+    merged_at: String,
+    merge_commit_sha: String
+}
+
+impl PrCommitData {
+  pub fn new(title: &str, merged_at: &str, merge_commit_sha: &str) -> PrCommitData {
+      PrCommitData {
+          title: title.to_string(),
+          merged_at: merged_at.to_string(),
+          merge_commit_sha: merge_commit_sha.to_string()
+      }
+  }
+  pub fn get_merge_commit_sha(&self) -> &str {
+      return &self.merge_commit_sha;
+  }
+}
+
 #[tokio::main]
-pub async fn poll_merge_commits(repo: &str, branch: &str) -> Result<(), Error> {
+pub async fn poll_merge_commits(repo: &str, branch: &str) -> Result<Vec<PrCommitData>, Error> {
 
   let mut path_string = String::from(repo);
   let query_params = format!("?state=closed&base={}&per_page=3&sort=updated&direction=desc", branch);
@@ -19,10 +39,21 @@ pub async fn poll_merge_commits(repo: &str, branch: &str) -> Result<(), Error> {
 
   let json_val: Vec<Value> = serde_json::from_str(&body).expect("badly formatted json");
 
+  // Simulate last deployed sha
+  let saved_sha: &str = "8534eac40f98f8680c216293e7c8c943d24142bb";
+
+  let mut commits_array: Vec<PrCommitData> = Vec::new();
+
   println!("\n\nNautilus is displaying undeployed commits for repo {}:\n", repo);
-  for i in json_val {
+  for i in &json_val {
+    if i["merge_commit_sha"] == saved_sha {
+      break;
+    }
+    let data = PrCommitData::new(&i["title"].to_string(), &i["merged_at"].to_string(), &i["merge_commit_sha"].to_string());
+
     println!("Title: {}\nMerged: {}\nMerge Commit Sha: {}\n", i["title"], i["merged_at"], i["merge_commit_sha"]);
+    commits_array.push(data);
   }
-  Ok(())
+  Ok(commits_array)
 }
 
