cd8e2f090d79cb2f9d02792698b4ed249b09a885
Dylan Leard <dylan.1.leard@gov.bc.ca>
2026-01-21 10:37:29 -0800

    feat: github modules return merge commits and statuses
    * chore: init github api feature branch
    
    * feat: add github.rs module to fetch merged PR commits from a github repo
    
    * chore: add some structs & implementation for returned data
    
    * feat: github modules return merge commits and statuses

---
+++ nautilus/src/github/merge_commits.rs
@@ -0,0 +1,25 @@
+// Function(s) to pull commit data from a defined github repo and branch
+
+
+use serde_json::Value;
+use serde_json;
+use reqwest::Error;
+use reqwest::header::{USER_AGENT};
+
+#[tokio::main]
+pub async fn get_merge_commits(repo: &str, branch: &str) -> Result<Vec<Value>, Error> {
+
+  let mut path_string = String::from(repo);
+  let query_params = format!("?state=closed&base={}&per_page=20&sort=updated&direction=desc", branch);
+  path_string.push_str("pulls");
+  path_string.push_str(&query_params);
+
+  let client = reqwest::Client::new();
+  let res = client.get(path_string).header(USER_AGENT, "Nautilus-2026").header("Accept", "application/vnd.github+json").header("X-GitHub-Api-Version", "2022-11-28").send();
+  let body = res.await.unwrap().text().await?;
+
+  let json_val: Vec<Value> = serde_json::from_str(&body).expect("badly formatted json");
+
+  Ok(json_val)
+}
+
---
+++ nautilus/src/github/status_checks_by_sha.rs
@@ -0,0 +1,24 @@
+// Function(s) to pull commit data from a defined github repo and branch
+
+
+use serde_json::Value;
+use serde_json;
+use reqwest::Error;
+use reqwest::header::{USER_AGENT};
+
+#[tokio::main]
+pub async fn get_status_checks_by_sha(repo: &str, sha: &str) -> Result<Value, Error> {
+
+  let mut path_string = String::from(repo);
+  let sha_string: &str = serde_json::from_str(sha).unwrap();
+  let api_path = format!("commits/{}/check-runs", sha_string);
+  path_string.push_str(&api_path);
+
+  let client = reqwest::Client::new();
+  let res = client.get(path_string).header(USER_AGENT, "Nautilus-2026").header("Accept", "application/vnd.github+json").header("X-GitHub-Api-Version", "2022-11-28").send();
+  let body = res.await.unwrap().text().await?;
+
+  let json_val: Value = serde_json::from_str(&body).expect("badly formatted json");
+
+  Ok(json_val)
+}
